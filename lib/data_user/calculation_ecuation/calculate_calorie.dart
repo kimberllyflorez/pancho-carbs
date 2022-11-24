ed329aa8ae68d61cc1f1deab059ff41ad72b30b9
kimberlly florez <kimberlly.florez@gmail.com>
2022-11-25 09:15:09 +1100

    modified base equation

--- lib/data_user/calculation_ecuation/calculate_calorie.dart
+++ lib/data_user/calculation_ecuation/calculate_calorie.dart
@@ -1,4 +1,5 @@
 import 'package:projects_study/data_user/calculation_ecuation/user_data.dart';
+import 'package:projects_study/data_user/model/user_data_model.dart';
 import 'package:projects_study/utils/gender.dart';
 
 import 'constant_equation.dart';
@@ -14,47 +15,47 @@ class CalculateCalorie {
     final geb = gebValue(dataUser);
     final eta = etaValue(geb);
     final get = getValue(eta, dataUser);
-
     return get;
   }
-}
 
-gebValue(dataUser) {
-  double geb = 0.0;
-  if (dataUser.gender == Gender.woman) {
-    geb = baseEquation(
+ double  gebValue(dataUser) {
+    double geb = 0.0;
+    if (dataUser.gender == Gender.woman) {
+      geb = baseEquation(
         ConstantsCalorieEquation.constantWoman,
         ConstantsCalorieEquation.heightWoman,
         ConstantsCalorieEquation.weightWoman,
         ConstantsCalorieEquation.ageWoman,
-        );
-  } else {
-    geb = baseEquation(
+        dataUser,
+      );
+    } else {
+      geb = baseEquation(
         ConstantsCalorieEquation.constantMan,
         ConstantsCalorieEquation.heightMan,
         ConstantsCalorieEquation.weightMan,
         ConstantsCalorieEquation.ageMan,
-        );
+        dataUser,
+      );
+    }
+    print('CALORIE: ${geb}');
+    return geb;
   }
-  print('CALORIE: ${geb}');
-  return geb;
+
+  double etaValue(get) {
+    return get * ConstantsCalorieEquation.eta;
+  }
+
+  double getValue(eta, dataUser) {
+    return eta * dataUser.activity;
+  }
+
+   baseEquation(constGender, constHeight, constWeight, constAge, UserDataModel dataUser) {
+
+
+    return (constGender * (constHeight * dataUser.height) +
+        (constWeight * dataUser.weight ) -
+        (constAge * dataUser.age)
+    );
+  }
 }
 
-etaValue(get) {
-  return get * ConstantsCalorieEquation.eta;
-}
-
-getValue(eta, dataUser) {
-  return eta * dataUser.activity;
-}
-
-double baseEquation(constGender, constHeight, constWeight, constAge) {
-final height = UserRepository().getHeight();
-final weight = UserRepository().getHeight();
-final age = UserRepository().getHeight();
-
- return (constGender * (constHeight * height) +
-      (constWeight * weight ) -
-      (constAge * age)
-  );
-}
